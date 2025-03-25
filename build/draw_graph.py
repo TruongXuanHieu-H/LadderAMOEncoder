4f20b4ca616bbdb56b1049a79396ff15119288c0
Hieu Xuan Truong <truongxuanhieu11@gmail.com>
2025-03-25 03:46:03 +0000

    fix draw

--- build/draw_graph.py
+++ build/draw_graph.py
@@ -23,12 +23,13 @@ def read_data(filename, step):
 
 
 # File name
-filename1 = 'ladder_report.txt'
-filename2 = 'duplex_report.txt'
-filename3 = 'product_report.txt'
-filename4 = 'reduced_report.txt'
-filename5 = 'naive_report.txt'
-filename6 = 'seq_report.txt'
+filename1 = 'reduced_report.txt'
+filename2 = 'seq_report.txt'
+filename3 = 'BDD_report.txt'
+filename4 = 'card_report.txt'
+filename5 = 'product_report.txt'
+filename6 = 'duplex_report.txt'
+filename7 = 'ladder_report.txt'
 
 # Read data
 x1, y1 = read_data(filename1, 5)
@@ -37,15 +38,17 @@ x3, y3 = read_data(filename3, 5)
 x4, y4 = read_data(filename4, 5)
 x5, y5 = read_data(filename5, 5)
 x6, y6 = read_data(filename6, 5)
+x7, y7 = read_data(filename7, 5)
 
 
 # Plot the data
-plt.plot(x4, y4, linestyle=':', color='blue', label="Reduced", linewidth=2)
-# plt.plot(x6, y6, linestyle='--', color='orange', label="Seq", linewidth=2)
-# plt.plot(x5, y5, linestyle='-', color='green', label="Naive", linewidth=2)
-# plt.plot(x3, y3, linestyle=':', color='red', label="Product", linewidth=2)
-plt.plot(x2, y2, linestyle='--', color='magenta', label="Duplex", linewidth=2)
-plt.plot(x1, y1, linestyle='-', color='black', label="SCL", linewidth=2)
+plt.plot(x1, y1, linestyle=':', color='blue', label="Pairwise", linewidth=2)
+plt.plot(x2, y2, linestyle='--', color='orange', label="Seq", linewidth=2)
+plt.plot(x3, y3, linestyle='-.', color='purple', label="BDD", linewidth=2)
+plt.plot(x4, y4, linestyle=':', color='green', label="Card", linewidth=2)
+plt.plot(x5, y5, linestyle='--', color='red', label="Product", linewidth=2)
+plt.plot(x6, y6, linestyle='-.', color='magenta', label="Duplex", linewidth=2)
+plt.plot(x7, y7, linestyle='-', color='black', label="SCL", linewidth=2)
 plt.legend()
 plt.xlabel('#Width')
 plt.ylabel('#Time (ms)')
